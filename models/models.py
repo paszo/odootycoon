f085468ea26af8973b03a2abecc9e8fde3794dc0
Paszo <paszo2008@gmail.com>
2019-01-21 21:05:32 +0100

    [ADD] Added model odootycoon_gamemanager

--- models/models.py
+++ models/models.py
@@ -2,14 +2,8 @@
 
 from odoo import models, fields, api
 
-# class odootycoon(models.Model):
-#     _name = 'odootycoon.odootycoon'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class odootycoon_gamemanager(models.Model):
+    _name = 'odootycoon.gamemanager'
+    name = fields.Char("Game Name")
+    day = fields.Integer("Current Day")
+    cash = fields.Float("Cash")
