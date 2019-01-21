fb00bfb70a3d9104a576eeafaccbd23133e6d3d0
Paszo <paszo2008@gmail.com>
2019-01-21 20:27:35 +0100

    [ADD] Added some little modification to manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -3,15 +3,14 @@
     'name': "odootycoon",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Create a fun Business Tycoon Game Using Odoo's Framework""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Greg Moss",
+    'website': "http://www.odooclass.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
