6d2c573e19aff7210a5593948ffee7467ed1b32f
Paszo <paszo2008@gmail.com>
2019-01-23 21:27:07 +0100

    [ADD] Added default values to the model

--- models/models.py
+++ models/models.py
@@ -4,6 +4,6 @@ from odoo import models, fields, api
 
 class odootycoon_gamemanager(models.Model):
     _name = 'odootycoon.gamemanager'
-    name = fields.Char("Game Name")
-    day = fields.Integer("Current Day")
-    cash = fields.Float("Cash")
+    name = fields.Char("Game Name", default="New Game")
+    day = fields.Integer("Current Day", default=1)
+    cash = fields.Float("Cash", default=5000)
