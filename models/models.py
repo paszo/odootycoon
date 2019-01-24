014a9bbe0ab1a956890da6da40b015dec8b55459
Paszo <paszo2008@gmail.com>
2019-01-24 21:27:13 +0100

    [ADD] Added button

--- models/models.py
+++ models/models.py
@@ -7,3 +7,11 @@ class odootycoon_gamemanager(models.Model):
     name = fields.Char("Game Name", default="New Game")
     day = fields.Integer("Current Day", default=1)
     cash = fields.Float("Cash", default=5000)
+
+    def nextday(self):
+        # Easy way ... bu two to the Postgres server
+        #self.day += 1
+        #self.cash -= 100
+
+        # This is the efficeint way
+        self.write({'day': self.day + 1, 'cash': self.cash - 100})
